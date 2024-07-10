a256e51cd8e5e46ef55ee1871930ce6fe5470654
Johannes Johansson <johannes.johansson@queenslab.se>
2024-07-10 13:56:53 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,27 +17,6 @@ def timer(func):
     return timeit_wrapper
 
 
-class TimerV1:
-    def __init__(self, func):
-        self.func = func
-        self.total_time = 0.0
-
-    def __call__(self, *args, **kwargs):
-        start_time = time.perf_counter()
-        try:
-            result = self.func(*args, **kwargs)
-        finally:
-            end_time = time.perf_counter()
-            elapsed_time = end_time - start_time
-            self.total_time += elapsed_time
-            print(
-                f"{self.func.__name__}: "
-                f"elapsed time: {elapsed_time:.6f} s, "
-                f"total time: {self.total_time:.6f} s"
-            )
-        return result
-
-
 class Timer:
     stats = {}
 
@@ -65,6 +44,10 @@ class Timer:
 
     @classmethod
     def report(cls):
+        """Prints the stats of all the decorated functions.
+
+        Can be extended to write to a file or replace the print statement with a logging statement.
+        """
         for key, value in cls.stats.items():
             print(f"Function: {key}")
             print(f"Number of Calls: {value['counter']}")
@@ -75,3 +58,24 @@ class Timer:
     @classmethod
     def reset_stats(cls):
         cls.stats = {}
+
+
+class TimerV1:
+    def __init__(self, func):
+        self.func = func
+        self.total_time = 0.0
+
+    def __call__(self, *args, **kwargs):
+        start_time = time.perf_counter()
+        try:
+            result = self.func(*args, **kwargs)
+        finally:
+            end_time = time.perf_counter()
+            elapsed_time = end_time - start_time
+            self.total_time += elapsed_time
+            print(
+                f"{self.func.__name__}: "
+                f"elapsed time: {elapsed_time:.6f} s, "
+                f"total time: {self.total_time:.6f} s"
+            )
+        return result
